8d6139c7ac79a997dc4c4cfffdacf41c62c11b9c
Gabriela Araujo <gabyara237@gmail.com>
2025-06-19 22:21:13 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Card from './components/Card';
+
+
+function App() {
+  return (
+    <>
+      <Card question="Hola" answer="jiji"/>
+    </>
+  )
+}
+
+export default App
